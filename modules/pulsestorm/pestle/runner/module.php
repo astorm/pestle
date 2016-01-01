1beb032a447f812a904a656ff8a1504aa8cb11e4
Alan Storm <astorm@alanstorm.com>
2015-12-31 17:37:47 -0800

    Issue #31

--- modules/pulsestorm/pestle/runner/module.php
+++ modules/pulsestorm/pestle/runner/module.php
@@ -148,11 +148,24 @@ function getListOfDefinedCliFunctions()
     return $commands;
 }
 
+function versionCheck()
+{
+    $version = phpversion();    
+    $version = '5.5.9';
+    if(version_compare($version, '5.6.0') === -1)
+    {
+        output("We're sorry, pestle requires PHP 5.6 or greater. It looks like " . 
+            "you're running " . $version . '.');
+        exit;    
+    }
+}
+
 /**
 * Main entry point
 */
 function main($argv)
 {
+    versionCheck();
     //include in the library for this command
     includeLibraryForCommand($argv);        
     $commands = getListOfDefinedCliFunctions();        
