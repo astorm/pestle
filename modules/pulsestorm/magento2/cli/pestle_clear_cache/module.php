4330ddfe37e780dd741f77aa34b6c141704de7af
Alan Storm <astorm@alanstorm.com>
2015-12-31 17:16:22 -0800

    Issue #32

---
+++ modules/pulsestorm/magento2/cli/pestle_clear_cache/module.php
@@ -0,0 +1,16 @@
+<?php
+namespace Pulsestorm\Magento2\Cli\Pestle_Clear_Cache;
+use function Pulsestorm\Pestle\Importer\pestle_import;
+pestle_import('Pulsestorm\Magento2\Cli\Library\output');
+pestle_import('pulsestorm\pestle\importer\getCacheDir');
+/**
+* One Line Description
+*
+* @command pestle_clear_cache
+*/
+function pestle_cli($argv)
+{
+    $cache_dir = getCacheDir();
+    rename($cache_dir, $cache_dir.'.'.time());
+    getCacheDir();
+}
