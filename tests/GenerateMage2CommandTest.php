efdde596b10cbfd58dd96b7018bed8805aebc908
Alan Storm <astorm@alanstorm.com>
2016-01-04 17:42:22 -0800

    Thanks TravisCi!

--- tests/GenerateMage2CommandTest.php
+++ tests/GenerateMage2CommandTest.php
@@ -2,7 +2,7 @@
 namespace Pulsestorm\Pestle\Tests;
 require_once 'PestleBaseTest.php';
 use function Pulsestorm\Pestle\Importer\pestle_import;
-pestle_import('Pulsestorm\Magento2\Cli\Generate\Mage2_Command\createNamespaceFromCommandName');
+pestle_import('Pulsestorm\Magento2\Cli\Generate\Mage2_Command\createNamespaceFromNamespaceAndCommandName');
         
 // use function Pulsestorm\Magento2\Cli\Generate\Mage2_Command\createNamespaceFromCommandName;
 
@@ -10,19 +10,19 @@ class GenerateMage2CommandTest extends PestleBaseTest
 {    
     public function testNamespaceGenerate1()
     {
-        $path = createNamespaceFromCommandName('science');        
+        $path = createNamespaceFromNamespaceAndCommandName('Pulsestorm\Magento2\Cli','science');        
         $this->assertEquals('Pulsestorm\Magento2\Cli\Science', $path);
     }
 
     public function testNamespaceGenerate2()
     {
-        $path = createNamespaceFromCommandName('generate_science');        
+        $path = createNamespaceFromNamespaceAndCommandName('Pulsestorm\Magento2\Cli','generate_science');        
         $this->assertEquals('Pulsestorm\Magento2\Cli\Generate\Science', $path);
     }
 
     public function testNamespaceGenerate3()
     {
-        $path = createNamespaceFromCommandName('foo_science');        
+        $path = createNamespaceFromNamespaceAndCommandName('Pulsestorm\Magento2\Cli','foo_science');        
         $this->assertEquals('Pulsestorm\Magento2\Cli\Foo_Science', $path);
     }        
 }
