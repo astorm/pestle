48b31322ff6693ec0194505d0187ed95a74623ff
Alan Storm <astorm@alanstorm.com>
2016-02-14 18:07:22 -0800

    Latest code

--- modules/pulsestorm/magento2/cli/testbed/module.php
+++ modules/pulsestorm/magento2/cli/testbed/module.php
@@ -3,17 +3,295 @@ namespace Pulsestorm\Magento2\Cli\Testbed;
 use function Pulsestorm\Pestle\Importer\pestle_import;
 pestle_import('Pulsestorm\Pestle\Library\output');
 pestle_import('Pulsestorm\Pestle\Library\input');
+pestle_import('Pulsestorm\Magento2\Cli\Library\getBaseMagentoDir');
+pestle_import('Pulsestorm\Xml_Library\formatXmlString');
+pestle_import('Pulsestorm\Cli\Token_Parse\pestle_token_get_all');
+
+function getFrontendModelNodesFromMagento1SystemXml($xmls)
+{
+    $items = [];
+    foreach($xmls as $xml_file)
+    {        
+        $xml = simplexml_load_file($xml_file);
+        $items[$xml_file] = [];        
+        foreach($xml->sections->children() as $section)
+        {
+            $strSection = $section->getName();
+            foreach($section->groups->children() as $group)
+            {
+                $strGroup = $group->getName();
+                foreach($group->fields->children() as $field)
+                {
+                    if($field->frontend_model)
+                    {
+                        $strField = $field->getName();
+                        $items[$xml_file][] = implode('/', 
+                        [$strSection, $strGroup, $strField]) . '::' . 
+                        (string) $field->frontend_model;
+                    }
+                }
+            }
+        }        
+    }
+    
+    return $items;
+}
+
+function getSectionXmlNodeFromSectionGroupAndField($xml,$section, $group, $field)
+{
+    $xpath = "/config/system/section[@id='$section']/group[@id='$group']/field[@id='$field']";
+    $nodes = $xml->xpath($xpath);
+    if(count($nodes) === 0)
+    {
+        throw new \Exception("Did no find node");
+    }
+    return array_shift($nodes);
+}
+
+function convertAliasToClass($alias)
+{
+    $map = [
+        'adminhtml/system_config_form_field_heading'
+            =>'Magento\Config\Block\System\Config\Form\Field\Heading',
+        'infortis/adminhtml_system_config_form_field_heading'
+            =>'Infortis\Infortis\Block\Adminhtml\System\Config\Form\Field\Heading',
+        'dataporter/system_config_form_field_configimpex'
+            =>'Infortis\Dataporter\Block\System\Config\Form\Field\Configimpex',
+        'ultimo/adminhtml_button_import_cms'
+            =>'Infortis\Ultimo\Block\Adminhtml\Button\Import\Cms',
+        'infortis/adminhtml_system_config_form_field_color'
+            =>'Infortis\Infortis\Block\Adminhtml\System\Config\Form\Field\Color',
+        'infortis/adminhtml_system_config_form_field_tex'
+            =>'Infortis\Infortis\Block\Adminhtml\System\Config\Form\Field\Tex'    
+    ];
+    
+    if(!array_key_exists($alias, $map))
+    {
+        output("What does $alias map to.\n");
+        return;
+    }
+    
+    return $map[$alias];
+}
+
+function backupOldCode($arguments, $options)
+{
+
+    $xmls = [
+        '/Users/alanstorm/Sites/magento-1-9-2-2.dev/app/code/local/Infortis/Brands/etc/system.xml',
+        '/Users/alanstorm/Sites/magento-1-9-2-2.dev/app/code/local/Infortis/CloudZoom/etc/system.xml',
+        '/Users/alanstorm/Sites/magento-1-9-2-2.dev/app/code/local/Infortis/Ultimo/etc/system.xml',
+        '/Users/alanstorm/Sites/magento-1-9-2-2.dev/app/code/local/Infortis/UltraMegamenu/etc/system.xml',
+        '/Users/alanstorm/Sites/magento-1-9-2-2.dev/app/code/local/Infortis/UltraSlideshow/etc/system.xml',    
+    ];
+    
+    $frontend_models = getFrontendModelNodesFromMagento1SystemXml($xmls);
+
+    foreach($frontend_models as $file=>$nodes)
+    {
+        $new_file = str_replace(
+            ['/Users/alanstorm/Sites/magento-1-9-2-2.dev','/local'],
+            '', $file);
+        $new_file = getBaseMagentoDir() . 
+            str_replace('/etc/', '/etc/adminhtml/', $new_file);            
+        
+        $xml = simplexml_load_file($new_file);
+        
+        foreach($nodes as $node)
+        {
+            list($path, $frontend_alias)   = explode('::', $node);
+            list($section, $group, $field) = explode('/', $path);
+            
+            $node = getSectionXmlNodeFromSectionGroupAndField($xml, 
+                $section, $group, $field);
+
+            if($node->frontend_model)
+            {
+                output("The frontend_model node already exists: " . $path);
+                continue;
+            }
+
+            $class = convertAliasToClass($frontend_alias);
+            $node->frontend_model = $class;
+        }
+        
+        file_put_contents($new_file, formatXmlString($xml->asXml()));
+    }
+    //search XML files
+    // $base = getBaseMagentoDir();
+    // $files = `find $base -name '*.xml'`;
+    // $files = preg_split('%[\r\n]%', $files);  
+    // $files = array_filter($files, function($file){
+    //     return strpos($file, '/view/') !== false &&
+    //     !is_dir($file);    
+    // });
+    // 
+    // $report;
+    // foreach($files as $file)
+    // {
+    //     $xml = simplexml_load_file($file);
+    //     if(!$xml->head){ continue; }
+    //     output($file);        
+    //     foreach($xml->head->children() as $node)
+    //     {
+    //         output('    ' . $node->getName());
+    //     }
+    // }
+  
+
+}
+
+define('PARSE_FOUND_NAMESPACE', 1);
+define('PARSE_FOUND_LINE_END', 2);
+define('PARSE_FOUND_USE', 3);
+function parseNamespaceFromTokens($tokens)
+{
+    $state = 0;
+    $all   = [];
+    $namespace_tokens = [];
+    foreach($tokens as $token)
+    {
+        if($token->token_name === 'T_NAMESPACE')
+        {
+            $state = PARSE_FOUND_NAMESPACE;
+        }
+        if($state === PARSE_FOUND_NAMESPACE && $token->token_value !== ';')
+        {
+            $namespace_tokens[] = $token;
+        }
+        if($state === PARSE_FOUND_NAMESPACE && $token->token_value === ';')
+        {
+            $state = PARSE_FOUND_LINE_END;
+            $all[] = $namespace_tokens;
+            $namespace_tokens = [];
+        }        
+    }
+    return $all;
+}
+
+function parseUsesFromTokens($tokens)
+{
+    $state = 0;
+    $all   = [];
+    $namespace_tokens = [];
+    foreach($tokens as $token)
+    {
+        if($token->token_name === 'T_USE')
+        {
+            $state = PARSE_FOUND_USE;
+        }
+        if($state === PARSE_FOUND_USE && $token->token_value !== ';')
+        {
+            $namespace_tokens[] = $token;
+        }
+        if($state === PARSE_FOUND_USE && $token->token_value === ';')
+        {
+            $state = PARSE_FOUND_LINE_END;
+            $all[] = $namespace_tokens;
+            $namespace_tokens = [];
+        }        
+    }
+    return $all;
+}
+
+function parseClassCodeFromTokens($tokens)
+{
+    //type hints in functions
+    
+    //after `new` keyword
+    
+    //directly before ::
+    
+    //in all of above, don't forget namespace seperator
+    print_r($tokens);
+    exit;
+}
+
+function parseSetupDiCompileReport()
+{
+    $contents   = file_get_contents('/Users/alanstorm/Dropbox/Untitled/Notes_2016-01-19_14-49-06');
+    $contents   = preg_match_all('%
+    Incorrect[ ]dependency[ ]in[ ]class[ ]
+    (.+?)
+    [ ]in[ ].+?php[\r\n](.+?)\t
+    %six', $contents, $matches, PREG_SET_ORDER);
+    
+    $report = [];
+    foreach($matches as $match)
+    {
+        if(!array_key_exists($match[1], $report))
+        {
+            $report[$match[1]] = [];
+        }
+        
+        $report[$match[1]] = array_merge($report[$match[1]], preg_split('%[\\r\n]%', $match[2]));
+    }
+    
+    foreach($report as $key=>$errors)
+    {
+        $errors = array_map(function($error){
+            $parts      = explode(' ', $error);
+            return array_shift($parts);
+        }, $errors);
+        
+        $errors = array_filter($errors, function($error){
+            $error = trim($error);
+            return !in_array($error, ['Total', 'Errors']);
+        });        
+        $report[$key]   = array_unique($errors);
+    }
+    
+    foreach($report as $key=>$errors)
+    {
+        output($key);
+        foreach($errors as $error)
+        {
+            output('    ' . $error);
+        }
+    }
+}
+
+function inProgressParsing()
+{
+    $contents   = file_get_contents('/Users/alanstorm/Documents/github_private/infortis-ultimo-magento2/app/code/Infortis/Brands/Block/Logo.php');
+    $tokens     = pestle_token_get_all($contents);
+    $namespace  = parseNamespaceFromTokens($tokens);    
+    // var_dump($namespace);
+    $uses       = parseUsesFromTokens($tokens);
+    // var_dump($uses);
+    
+    $classes    = parseClassCodeFromTokens($tokens);
+}
 
 /**
 * Test Command
 * @command testbed
-* @argument argument_name Please enter a value for argument_name [the default value]
-* @argument argument_foo Please enter a value for argument_name [the default value]
 */
 function pestle_cli($arguments, $options)
 {
-    foreach($arguments as $key=>$value)
+    // inProgressParsing();
+    
+    $urls = [
+        "http://stackoverflow.com/questions/5412950/how-would-i-pull-the-content-of-a-cms-page-into-a-static-block/5413698",
+        "http://stackoverflow.com/questions/5412950/how-would-i-pull-the-content-of-a-cms-page-into-a-static-block",
+        "http://topwebseiten.de/mage-news.de",
+        "http://www.venchina.com/noticia/venezuela/2014-01-13/141565.html",
+        "http://www.venchina.com/noticia/ent/index_181.html",
+        "http://www.venchina.com/noticia/china/2013-03-09/136348.html",
+        "http://forum.azmagento.com/magento-users-guide/how-would-i-pull-the-content-of-a-cms-page-into-a-static-block-3267.html",
+        "http://www.mage-news.de/startseite?page=128",
+        "http://www.mage-news.de/englische-news?page=88",
+        "http://answerlists.com/question/137218/how-would-i-pull-the-content-of-a-cms-page-into-a-static-block",
+        "http://gootomain.com/question/137218/how-would-i-pull-the-content-of-a-cms-page-into-a-static-block",
+    ];
+    foreach($urls as $url)
     {
-        output($key . '::' . $value);
-    }    
+        $html = `curl $url`;
+        $html = str_replace('>',">\n",$html);
+        preg_match_all('%^.*alanstorm\.com.*$%m',$html, $matches);    
+        foreach($matches[0] as $match)
+        {
+            echo $match,"\n";
+        }
+    }
 }
