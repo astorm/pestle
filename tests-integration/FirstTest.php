ce5cfe4ff9ef8424b3124e2afa9663eefc510c81
Alan Storm <astorm@alanstorm.com>
2016-03-21 21:25:13 -0700

    Adding integration test.

--- tests-integration/FirstTest.php
+++ tests-integration/FirstTest.php
@@ -1,8 +1,14 @@
 <?php 
 class FirstTest extends PHPUnit_Framework_TestCase
 {
+    protected function runCommand($cmd)
+    {
+        return `pestle.phar $cmd`;
+    }
+    
     public function testSetup()
     {
-        $this->assertEquals(1,1);
+        $results = trim($this->runCommand('hello_world'));
+        $this->assertEquals($results,'Hello Sailor');
     }
 }
