1fc821c31dba5e97ebfb99bacf9769f935bae9da
Alan Storm <astorm@alanstorm.com>
2016-04-28 00:29:58 -0700

    Issue #137

--- modules/pulsestorm/magento2/cli/generate/acl/module.php
+++ modules/pulsestorm/magento2/cli/generate/acl/module.php
@@ -6,6 +6,8 @@ pestle_import('Pulsestorm\Magento2\Cli\Library\getBaseModuleDir');
 pestle_import('Pulsestorm\Magento2\Cli\Xml_Template\getBlankXml');
 pestle_import('Pulsestorm\Xml_Library\formatXmlString');
 pestle_import('Pulsestorm\Pestle\Library\writeStringToFile');
+pestle_import('Pulsestorm\Xml_Library\simpleXmlAddNodesXpath');
+
 /**
 * One Line Description
 *
@@ -19,18 +21,23 @@ function pestle_cli($argv)
     $rule_ids = explode(',', $rule_ids);
     $rule_ids = array_filter($rule_ids);
     
-    $xml = simplexml_load_string(getBlankXml('acl'));
-    $nodes = $xml->xpath('//resource[@id="Magento_Backend::admin"]');
-    $node  = array_shift($nodes);           
+    $path = getBaseModuleDir($module_name) . '/etc/acl.xml';
+    if(!file_exists($path))
+    {
+        $xml = simplexml_load_string(getBlankXml('acl'));
+        writeStringToFile($path, $xml->asXml());
+    }    
+    $xml = simplexml_load_file($path);
+    
+    $xpath = 'acl/resources/resource[@id=Magento_Backend::admin]';
+    
     foreach($rule_ids as $id)
     {        
         $id = trim($id);
-        $node = $node->addChild('resource');
-        $node->addAttribute('id', $id);
-        $node->addAttribute('title', 'TITLE HERE FOR ' . $id);
+        $xpath .= '/resource[@id='.$id.',@title=TITLE HERE FOR]';
     }
+    simpleXmlAddNodesXpath($xml,$xpath);
     
-    $path = getBaseModuleDir($module_name) . '/etc/acl.xml';
     writeStringToFile($path, formatXmlString($xml->asXml()));
     output("Created $path");
 }
