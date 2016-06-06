c1e47c4c3cfa19fad1960d5f7fb3b1b40a38b416
Alan Storm <astorm@alanstorm.com>
2016-06-06 11:54:12 -0700

    Janky paypal script.

--- modules/pulsestorm/paypal/csv_to_iif/module.php
+++ modules/pulsestorm/paypal/csv_to_iif/module.php
@@ -47,18 +47,57 @@ function processPaypal($line)
         return;
     }
     $row = joinHeadersAndValue($headers, $line);
-    var_dump($row);
-        
+    if(strpos($row['Type'],'Transfer to Bank') !== false)
+    {
+        return null;
+    }
+    $iif      = getIifTemplate();
+    $iif      = str_replace('<$date$>',         $row['Date'],       $iif);
+    $iif      = str_replace('<$entity$>',       $row['Name'],       $iif);
+    $iif      = str_replace('<$amount$>',       trim($row['Net']),        $iif);
     
+    $product_title = $row['Item Title'];
+    if(!$product_title)
+    {
+        $product_title = $row['Subject'];
+    }
+    
+    //dupe "no title" behavior
+    if($product_title)
+    {
+        $iif      = str_replace('<$product_name$>', $product_title, $iif);
+    }
+    else
+    {
+        $iif      = str_replace('"<$product_name$>"' . "\t", '', $iif);
+    }
+    
+    $iif      = str_replace('<$amount_full$>',  number_format(($row['Gross'] * -1),2), $iif);    
+    $iif      = str_replace('<$amount_fee$>',   number_format(($row['Fee'] * -1),2), $iif);        
+
+    if((int) $row['Fee'] === 0)
+    {
+        $parts = preg_split('%[\r\n]{1,2}%', $iif);
+        $parts = array_filter($parts, function($item){
+            return strpos($item, '"Bank Fee"') === false;
+        });
+        
+        $iif = implode("\n",$parts);
+        if(strpos($iif, '"Bank Fee"') === false)
+        {
+            $iif = str_replace('Express Checkout Payment Received', 'Payment Received', $iif);
+        }                 
+    }
+    return $iif;    
 }
 
 function getIifTemplate()
 {
-    $template = 'TRNS	"<$date$>"	"Paypal"	"<$entity$>"	"<$Express Checkout Payment Received$>"	<$amount$>	"<$No Frills Magento Layout - No Frills Magento Layout: Single User License$>"	
-SPL	"6/29/2015"	"Sales-Software"	"<$entity$>"	-<$amount_full$>
-SPL	"6/29/2015"	"Bank Fee"	Fee	<$amount_fee$>
+    $template = 'TRNS	"<$date$>"	"Paypal"	"<$entity$>"	"Express Checkout Payment Received"	<$amount$>	"<$product_name$>"	
+SPL	"<$date$>"	"Sales-Software"	"<$entity$>"	<$amount_full$>
+SPL	"<$date$>"	"Bank Fee"	Fee	<$amount_fee$>
 ENDTRNS';
-
+    return $template;
 }
 
 /**
@@ -72,13 +111,19 @@ function pestle_cli($argv)
     extract($argv);
     $handle = fopen($path_to_file, 'r');
     $process_function = false;
+    $iifs = [];
     while($line = fgetcsv($handle))
     {
         if(!$process_function)
         {
             $process_function = getProcessFunctionFromFirstLine($line);
         }        
-        call_user_func($process_function, $line);
+        $iifs[] = call_user_func($process_function, $line);
     }
-    output("Hello Sailor");
+    $iifs = array_filter($iifs);
+    $iifs = array_reverse($iifs);
+    output('!TRNS	DATE	ACCNT	NAME	CLASS	AMOUNT	MEMO
+!SPL	DATE	ACCNT	NAME	AMOUNT	MEMO
+!ENDTRNS');
+    output(implode("\n", $iifs));
 }
