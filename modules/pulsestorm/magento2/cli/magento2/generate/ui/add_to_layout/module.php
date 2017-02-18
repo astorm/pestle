2d507de3fee7a59d3d62c86b1293a2057cc2d5b5
Alan Storm <astorm@alanstorm.com>
2017-02-17 18:13:29 -0800

    Issue #191 -- add a uiComponent to a layout file

---
+++ modules/pulsestorm/magento2/cli/magento2/generate/ui/add_to_layout/module.php
@@ -0,0 +1,69 @@
+<?php
+namespace Pulsestorm\Magento2\Cli\Magento2\Generate\Ui\Add_To_Layout;
+use function Pulsestorm\Pestle\Importer\pestle_import;
+pestle_import('Pulsestorm\Pestle\Library\output');
+pestle_import('Pulsestorm\Xml_Library\formatXmlString');
+pestle_import('Pulsestorm\Pestle\Library\writeStringToFile');
+
+function exitWithErrorMessage($message)
+{
+    fwrite(STDERR, $message . "\n");
+    exit(1);
+}
+
+function validateNoSuchComponent($xml, $name)
+{
+    $nodes = getNamedXmlBlockWithNodeNames($xml, $name, ['uiComponent']);
+    if(count($nodes) === 0)
+    {
+        return;
+    }
+    exitWithErrorMessage("Bailing: uiComponent Node Already Exists");
+}
+
+function getContentBlockOrContainerOrReference($xml, $name)
+{
+    return getNamedXmlBlockWithNodeNames($xml, $name, 
+        ['container', 'block', 'referenceContainer','referenceBlock']);
+}
+
+function getNamedXmlBlockWithNodeNames($xml, $name, $names)
+{
+    $search = '//*[@name="' . $name . '"]';
+    $nodes = $xml->xpath($search);
+    $nodes = array_filter($nodes, function($node) use ($names){
+        return in_array($node->getName(), $names);
+    });
+    return $nodes;
+}
+
+function getContentNode($xml,$argv)
+{
+    $nodes = getContentBlockOrContainerOrReference($xml, $argv['block_name']);    
+    if(count($nodes) > 1)
+    {
+        exitWithErrorMessage("BAILING: Found more than one name=\"".$argv['block_name']."\" node.\n");
+    }
+    return array_pop($nodes);
+}
+
+/**
+* One Line Description
+*
+* @command magento2:generate:ui:add_to_layout
+* @argument path_layout Layout XML File?
+* @argument block_name Block or Reference Name?
+* @argument ui_component_name UI Component Name?
+*/
+function pestle_cli($argv)
+{
+    $xml    = simplexml_load_file($argv['path_layout']);    
+    validateNoSuchComponent($xml, $argv['ui_component_name']);
+    $node   = getContentNode($xml, $argv);
+    
+    $xml->addChild('uiComponent')
+        ->addAttribute('name', $argv['ui_component_name']);
+    $xmlString    = formatXmlString($xml->asXml());        
+    writeStringToFile($argv['path_layout'], $xmlString);        
+    output("Added Component");
+}
