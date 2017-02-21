5234efeb270db69b893bd623a602e07a767e6475
Alan Storm <astorm@alanstorm.com>
2017-02-21 15:19:51 -0800

    Issue #202

---
+++ modules/pulsestorm/pestle/version/module.php
@@ -0,0 +1,14 @@
+<?php
+namespace Pulsestorm\Pestle\Version;
+use function Pulsestorm\Pestle\Importer\pestle_import;
+pestle_import('Pulsestorm\Pestle\Library\output');
+define('PULSESTORM_PESTLE_VERSION', '1.1.1');
+/**
+* One Line Description
+*
+* @command version
+*/
+function pestle_cli($argv)
+{
+    output('pestle Ver ' . PULSESTORM_PESTLE_VERSION);
+}
