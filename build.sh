dbbebb346910f0c4dedfe70d00081cbe292847d5
Alan Storm <astorm@alanstorm.com>
2017-02-21 15:33:31 -0800

    Issue #202

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+VER_FROM_TAG=`git tag | tail -n 1 | xargs echo 'pestle Ver'`
+VER_FROM_PESTLE=`pestle_dev version`
+
+if [ "$VER_FROM_TAG" != "$VER_FROM_PESTLE" ]
+then
+  echo "pestle version and latest tag do not match, bailing"
+  exit
+fi
+
 php -d phar.readonly=0 vendor/bin/phing package_phar
 chmod +x pestle.phar
 #mv pestle.phar ~/bin/pestle.phar
--- modules/pulsestorm/pestle/version/module.php
+++ modules/pulsestorm/pestle/version/module.php
@@ -2,7 +2,7 @@
 namespace Pulsestorm\Pestle\Version;
 use function Pulsestorm\Pestle\Importer\pestle_import;
 pestle_import('Pulsestorm\Pestle\Library\output');
-define('PULSESTORM_PESTLE_VERSION', '1.1.1');
+define('PULSESTORM_PESTLE_VERSION', '1.1.2');
 /**
 * One Line Description
 *
