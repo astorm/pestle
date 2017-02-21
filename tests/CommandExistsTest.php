fbd697d950cff1953e2581039c3728ea8713384e
Alan Storm <astorm@alanstorm.com>
2017-02-20 18:49:42 -0800

    Fixing tests that reference old command names.

--- tests/CommandExistsTest.php
+++ tests/CommandExistsTest.php
@@ -11,80 +11,80 @@ class CommandExistsTest extends PestleBaseTest
 
     function testCommandsExistbaz_bar()
     {
-        $results = $this->runHelpCommand('baz_bar');
+        $results = $this->runHelpCommand('pestle:baz_bar');
         $this->assertContains('Help:', $results);    
         
     }
     
     function testCommandsExistbuild_command_list()
     {
-        $results = $this->runHelpCommand('build_command_list');
+        $results = $this->runHelpCommand('pestle:build_command_list');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistcheck_class_and_namespace()
     {
-        $results = $this->runHelpCommand('check_class_and_namespace');
+        $results = $this->runHelpCommand('magento2:scan:class_and_namespace');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistcheck_htaccess()
     {
-        $results = $this->runHelpCommand('check_htaccess');
+        $results = $this->runHelpCommand('magento2:scan:htaccess');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistcheck_registration()
     {
-        $results = $this->runHelpCommand('check_registration');
+        $results = $this->runHelpCommand('magento2:scan:registration');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistcheck_templates()
     {
-        $results = $this->runHelpCommand('check_templates');
+        $results = $this->runHelpCommand('magento2:check_templates');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistclass_from_path()
     {
-        $results = $this->runHelpCommand('class_from_path');
+        $results = $this->runHelpCommand('magento2:class_from_path');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistconvert_class()
     {
-        $results = $this->runHelpCommand('convert_class');
+        $results = $this->runHelpCommand('magento2:convert_class');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistconvert_observers_xml()
     {
-        $results = $this->runHelpCommand('convert_observers_xml');
+        $results = $this->runHelpCommand('magento2:convert_observers_xml');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistconvert_selenium_id_for_codecept()
     {
-        $results = $this->runHelpCommand('convert_selenium_id_for_codecept');
+        $results = $this->runHelpCommand('codecept:convert_selenium_id_for_codecept');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistconvert_system_xml()
     {
-        $results = $this->runHelpCommand('convert_system_xml');
+        $results = $this->runHelpCommand('magento2:convert_system_xml');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistdev_import()
     {
-        $results = $this->runHelpCommand('dev_import');
+        $results = $this->runHelpCommand('pestle:dev_import');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistdev_namespace()
     {
-        $results = $this->runHelpCommand('dev_namespace');
+        $results = $this->runHelpCommand('pestle:dev_namespace');
         $this->assertContains('Help:', $results);    
         
     }
@@ -96,19 +96,19 @@ class CommandExistsTest extends PestleBaseTest
     }
     function testCommandsExistextract_mage2_system_xml_paths()
     {
-        $results = $this->runHelpCommand('extract_mage2_system_xml_paths');
+        $results = $this->runHelpCommand('magento2:extract_mage2_system_xml_paths');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistextract_session()
     {
-        $results = $this->runHelpCommand('extract_session');
+        $results = $this->runHelpCommand('php:extract_session');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistfix_permissions_modphp()
     {
-        $results = $this->runHelpCommand('fix_permissions_modphp');
+        $results = $this->runHelpCommand('magento2:fix_permissions_modphp');
         $this->assertContains('Help:', $results);    
         
     }
@@ -207,19 +207,19 @@ class CommandExistsTest extends PestleBaseTest
     }
     function testCommandsExistmd_to_say()
     {
-        $results = $this->runHelpCommand('md_to_say');
+        $results = $this->runHelpCommand('pulsestorm:md_to_say');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistpath_from_class()
     {
-        $results = $this->runHelpCommand('path_from_class');
+        $results = $this->runHelpCommand('magento2:path_from_class');
         $this->assertContains('Help:', $results);    
         
     }
     function testCommandsExistsearch_controllers()
     {
-        $results = $this->runHelpCommand('search_controllers');
+        $results = $this->runHelpCommand('magento2:search:search_controllers');
         $this->assertContains('Help:', $results);    
         
     }
