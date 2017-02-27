74a35f2e3c4b419fd5ca38dde54667f37d334583
Alan Storm <astorm@alanstorm.com>
2017-02-27 10:48:06 -0800

    Need to build and then tag.

--- build-library.bash
+++ build-library.bash
@@ -2,11 +2,11 @@
 VER_FROM_TAG=`git tag | tail -n 1 | xargs echo 'pestle Ver'`
 VER_FROM_PESTLE=`pestle_dev version`
 
-if [ "$VER_FROM_TAG" != "$VER_FROM_PESTLE" ]
-then
-  echo "pestle version and latest tag do not match, bailing"
-  exit
-fi
+#if [ "$VER_FROM_TAG" != "$VER_FROM_PESTLE" ]
+#then
+#  echo "pestle version and latest tag do not match, bailing"
+#  exit
+#fi
 
 echo "<?php" > library/all.php
 find modules/ -name module.php | xargs pestle_dev pestle:export_module >> library/all.php
