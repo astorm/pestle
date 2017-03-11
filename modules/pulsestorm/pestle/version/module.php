24984232069e95636216b3686a83653a9db65854
Alan Storm <astorm@alanstorm.com>
2017-03-10 17:17:34 -0800

    Tag Snafu

--- modules/pulsestorm/pestle/version/module.php
+++ modules/pulsestorm/pestle/version/module.php
@@ -2,7 +2,7 @@
 namespace Pulsestorm\Pestle\Version;
 use function Pulsestorm\Pestle\Importer\pestle_import;
 pestle_import('Pulsestorm\Pestle\Library\output');
-define('PULSESTORM_PESTLE_VERSION', '1.1.2');
+define('PULSESTORM_PESTLE_VERSION', '1.2.1');
 /**
 * One Line Description
 *
