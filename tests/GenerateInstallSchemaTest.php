92f0e61c8a57fbb1b69e77b8e571b1d0583b541d
Michel Sallaberry <michel@web-art-factory.com>
2019-08-09 03:40:39 +0200

    test remove trailing comma inline array declaration

--- tests/GenerateInstallSchemaTest.php
+++ tests/GenerateInstallSchemaTest.php
@@ -22,31 +22,31 @@ class GenerateInstallSchemaTest extends PestleBaseTest
             \'modeltest_id\',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
             null,
-            [ \'identity\' => true, \'nullable\' => false, \'primary\' => true, \'unsigned\' => true, ],
+            [ \'identity\' => true, \'nullable\' => false, \'primary\' => true, \'unsigned\' => true ],
             \'Entity ID\'
         )->addColumn(
             \'title\',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
             255,
-            [ \'nullable\' => false, ],
+            [ \'nullable\' => false ],
             \'Demo Title\'
         )->addColumn(
             \'creation_time\',
             \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
             null,
-            [ \'nullable\' => false, \'default\' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT, ],
+            [ \'nullable\' => false, \'default\' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT ],
             \'Creation Time\'
         )->addColumn(
             \'update_time\',
             \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
             null,
-            [ \'nullable\' => false, \'default\' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE, ],
+            [ \'nullable\' => false, \'default\' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE ],
             \'Modification Time\'
         )->addColumn(
             \'is_active\',
             \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
             null,
-            [ \'nullable\' => false, \'default\' => \'1\', ],
+            [ \'nullable\' => false, \'default\' => \'1\' ],
             \'Is Active\'
         )->setComment(
              \'A testing table\'
