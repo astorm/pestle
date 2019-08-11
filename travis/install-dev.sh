0e4d12f56612ac8b14c9184347e3fc6292e466ef
Alan Storm <astorm@alanstorm.com>
2019-08-10 17:07:12 -0700

    try try again

---
+++ travis/install-dev.sh
@@ -0,0 +1,5 @@
+#!/usr/bash
+sudo printf '#!/usr/bin/env php \n<?php\nrequire "' > /usr/bin/pestle_dev
+sudo printf `pwd` >> /usr/bin/pestle_dev
+sudo printf '/runner.php";' >> /usr/bin/pestle_dev
+sudo chmod +x /usr/bin/pestle_dev
