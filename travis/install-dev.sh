af63531f6c419e8b011b496cfcebc3b4897aaac6
Alan Storm <astorm@alanstorm.com>
2019-08-10 17:18:14 -0700

    slow and steady

--- travis/install-dev.sh
+++ travis/install-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo printf '#!/usr/bin/env php \n<?php\nrequire "' > /usr/bin/pestle_dev
-sudo printf `pwd` >> /usr/bin/pestle_dev
+sudo printf $1 >> /usr/bin/pestle_dev
 sudo printf '/runner.php";' >> /usr/bin/pestle_dev
 sudo chmod +x /usr/bin/pestle_dev
