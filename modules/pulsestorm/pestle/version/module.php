ea45dbea5b766acfe1236e8452282524cd6055dc
Alan Storm <astorm@alanstorm.com>
2020-02-09 15:03:11 -0800

    1.5.1 release

--- modules/pulsestorm/pestle/version/module.php
+++ modules/pulsestorm/pestle/version/module.php
@@ -2,7 +2,7 @@
 namespace Pulsestorm\Pestle\Version;
 use function Pulsestorm\Pestle\Importer\pestle_import;
 pestle_import('Pulsestorm\Pestle\Library\output');
-define('PULSESTORM_PESTLE_VERSION', '1.5.0');
+define('PULSESTORM_PESTLE_VERSION', '1.5.1');
 /**
 * Displays Pestle Version
 *
